36189085b531349689c12a5f7573116e2ee6b227
marykn <45259255+marykn@users.noreply.github.com>
2019-11-02 04:20:00 -0700

    Add files via upload

---
+++ Datee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using GSD.Globalization;
+
+namespace WebApplication19
+{
+    public class Datee
+    {
+      
+
+        private void DateBtn_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            PersianCalendar jc = new PersianCalendar();
+            string year = jc.GetYear(dt).ToString();
+            string month = jc.GetMonth(dt).ToString();
+            string day = jc.GetDayOfMonth(dt).ToString();
+            string date = String.Format("{0}/{1}/{2}", year, month, day);
+            txtDate2.Text = date;
+        }
+    }
+}
---
+++ PersianCulture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Reflection;
+
+namespace GSD.Globalization
+{
+    public class PersianCulture
+    {
+        private void date2Btn_Click(object sender, EventArgs e)
+        {
+            PersianCalendar jc = new PersianCalendar();
+            DateTime dt = jc.ToDateTime(1393, 3, 1, 0, 0, 0, 0);
+            txtDate2.Text = dt.ToShortDateString();
+        }
+    }
+
+    internal class txtDate2
+    {
+        public static string Text { get; internal set; }
+    }
+}
